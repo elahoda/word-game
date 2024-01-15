eb03e14a99c26fccdaea8527aa0d8c6cddcf1406
Eric Lahoda <elahoda@gmail.com>
2024-01-15 16:39:22 -0500

    type equiv inApp.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
 
   const checkAnswer = (option, optionIndex, correctAnswer) => {
     console.log(optionIndex, correctAnswer);
-    if (optionIndex == correctAnswer) {
+    if (optionIndex === correctAnswer) {
       setCorrectAnswers([...correctAnswers, option]);
       setScore((score) => score + 1);
     } else {
